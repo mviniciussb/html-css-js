a92e41abe246aeff5947c81a7df1121b78293fe6
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-05-28 15:52:27 -0300

    randomização das frases

--- 2-BiscoitoSorte/main.js
+++ 2-BiscoitoSorte/main.js
@@ -1,16 +1,34 @@
 // Variaveis
+let randomNumberPhrase = Math.round(Math.random() * 10)
 const cookieClick = document.querySelector("#cookieClick")
 const screenOne = document.querySelector("#screenOne")
 const screenTwo = document.querySelector("#screenTwo")
 const openNewCookie = document.querySelector("#openNewCookie")
+const phrasesList = [
+"Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+"A vida trará coisas boas se tiver paciência.",
+"Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+"Não compense na ira o que lhe falta na razão.",
+"Defeitos e virtudes são apenas dois lados da mesma moeda.", 
+"A maior de todas as torres começa no solo.", 
+"Não há que ser forte. Há que ser flexível.", 
+"Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?", 
+"Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.", 
+"A juventude não é uma época da vida, é um estado de espírito."]
+let phrase
 
 // Eventos
 cookieClick.addEventListener("click", luckyPhrase)
-openNewCookie.addEventListener("click", openCokie)
+openNewCookie.addEventListener("click", openNewCokie)
 
 // Funções
 function luckyPhrase(){
     toggleScreen()
+
+    phrase = phrasesList[randomNumberPhrase]
+    
+
+    screenTwo.querySelector("p").innerText = phrase
 }
 
 function toggleScreen(){
@@ -18,6 +36,7 @@ function toggleScreen(){
     screenTwo.classList.toggle("hide")
 }
 
-function openCokie(){
+function openNewCokie(){
     toggleScreen()
+    randomNumberPhrase = Math.round(Math.random() * 10)
 }
