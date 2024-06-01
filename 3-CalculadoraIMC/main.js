7d6dcfbcc0e63f3109ebf96d0ab99b008a58a421
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-01 11:10:07 -0300

    Update main.js

--- 3-CalculadoraIMC/main.js
+++ 3-CalculadoraIMC/main.js
@@ -2,9 +2,6 @@
 const formButton = document.querySelector("#formButton")
 let weight = document.querySelector("#weight")
 let height = document.querySelector("#height")
-const modalWrapper = document.querySelector(".modal-wrapper")
-const buttonClose = document.querySelector(".close")
-const  modalCard = document.querySelector(".modal-card")
 
 // Events
 formButton.addEventListener("click", handleForm)
@@ -14,19 +11,6 @@ buttonClose.addEventListener("click", closeModal)
 function handleForm(event) {
     event.preventDefault()
 
-    const inputWeigth = Number(weight.value)
-    const inputHeigth = Number(height.value)
-
-    const result = calcImc(inputWeigth, inputHeigth)
-    
-    modalCard.querySelector("h2").innerText = `Seu IMC é de ${result}`
-    modalWrapper.classList.add("openPopUp")
-}
-function calcImc(weight, height) {
-    return (weight / (height / 100) ** 2).toFixed(2)
-}
-function closeModal(){
-    if(modalWrapper.classList.contains("openPopUp")){
-        modalWrapper.classList.toggle("openPopUp")
-    }
+    const inputWeigth = weight.value
+    const inputHeigth = height.value
 }
