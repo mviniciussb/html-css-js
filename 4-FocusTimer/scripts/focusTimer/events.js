37058f9e00ada8174a0a7b8ad0963ef680830c4a
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-05 11:46:05 -0300

    a

--- 4-FocusTimer/scripts/focusTimer/events.js
+++ 4-FocusTimer/scripts/focusTimer/events.js
@@ -2,6 +2,12 @@ import { controlsPlay } from "./elements.js"
 
 export function registerControls() {
     controlsPlay.addEventListener("click", (event) => {
-        console.log(event.target)
+        const action = event.target.dataset.action
+
+        if(action === undefined){
+            return
+        }
+
+        console.log(action)
     })
 }
