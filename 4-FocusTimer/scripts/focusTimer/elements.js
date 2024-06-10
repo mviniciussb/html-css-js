d1dd2d049d4455a2586a01982932fa35fbba16a9
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-10 10:14:56 -0300

    .

--- 4-FocusTimer/scripts/focusTimer/elements.js
+++ 4-FocusTimer/scripts/focusTimer/elements.js
@@ -1,1 +1,4 @@
 export const controls = document.querySelector(".controlsPlay")
+
+export const minutes = document.querySelector(".minutes")
+export const seconds = document.querySelector(".seconds")
