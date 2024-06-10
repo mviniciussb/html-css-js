d3691ffd31d31311cc92593563acf6ae044a0cc2
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-10 08:18:52 -0300

    .

--- 4-FocusTimer/scripts/focusTimer/elements.js
+++
@@ -1,5 +0,0 @@
-export const controlsPlay = document.querySelector(".controlsPlay")
-
-export const minutesElement = document.querySelector(".minutes")
-
-export const secondsElement = document.querySelector(".seconds")
--- 4-FocusTimer/scripts/focusTimer/state.js
+++
@@ -1,6 +0,0 @@
-export default {
-    minutesTime: 25,
-    secondsTime: 0,
-    isRunning: false,
-    isMute: true
-}
