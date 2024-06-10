e967566b7026ab21e010e582f28fdb25933a28d3
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-10 08:50:47 -0300

    ,

--- 4-FocusTimer/scripts/focusTimer/events.js
+++ 4-FocusTimer/scripts/focusTimer/events.js
@@ -1,3 +1,7 @@
-export function registerControls(){
-    console.log("register")
+import { controls } from "./elements.js"
+
+export function registerControls() {
+    controls.addEventListener("click", (event) => {
+        console.log(event.target)
+    })
 }
