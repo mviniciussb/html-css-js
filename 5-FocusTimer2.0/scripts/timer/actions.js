ce054c71934e086a9e7dc84155f19232564c6314
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-11 15:50:52 -0300

    adicionando o timer

---
+++ 5-FocusTimer2.0/scripts/timer/actions.js
@@ -0,0 +1,13 @@
+import state from "./state.js"
+
+export function toggleRunning(){
+    
+}
+
+export function addTime(){
+    state.minutes = 5
+}
+
+export function subTime(){
+    state.minutes = -5
+}
--- 5-FocusTimer2.0/scripts/timer/events.js
+++ 5-FocusTimer2.0/scripts/timer/events.js
@@ -1,10 +1,14 @@
-import * as elements from "./elements.js"
 import * as actions from "./actions.js"
 
 
 
 export function setControlFunction() {
     controls.addEventListener("click", (event) => {
-        const registerControls = event.target.dataset.action
+        const action = event.target.dataset.action
+
+        if(typeof actions[action] != "function"){
+            return
+        }
+        actions[action]()
     })
 }
