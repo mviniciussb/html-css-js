c7022f05f9cd63f8128bcba0c6e9cc1ae124b2d7
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-11 16:19:57 -0300

    .

--- 5-FocusTimer2.0/scripts/timer/actions.js
+++ 5-FocusTimer2.0/scripts/timer/actions.js
@@ -2,13 +2,12 @@ import state from "./state.js"
 import * as elements from "./elements.js"
 
 export function toggleRunning(){
-    
+    // trabalhar aqui
 }
 
 export function addTime(){
     state.minutes += 5
     elements.minutes.textContent = state.minutes
-    
 }
 
 export function subTime(){
---
+++ 5-FocusTimer2.0/scripts/timer/countdown.js
@@ -0,0 +1,5 @@
+import * as elements from "./elements.js"
+import * as events from "./events.js"
+import state from "./state.js"
+import * as actions from "./actions.js"
+
