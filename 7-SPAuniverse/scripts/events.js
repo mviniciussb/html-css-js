15fdb279d997a1086265d52bbf6b88c77a0150a0
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-18 14:39:04 -0300

    adicionado planos de fundo

--- 7-SPAuniverse/scripts/events.js
+++ 7-SPAuniverse/scripts/events.js
@@ -1,29 +1,43 @@
 import * as elements from "./elements.js"
 import * as routes from "./routes.js"
 
-export function toggleScreen(){
+export function toggleScreen() {
+
+
     elements.home.addEventListener("click", (event) => {
         event.preventDefault()
-        
+
+        elements.BackgroundScreen.style.backgroundImage =
+            "url('./assets/mountains-universe-1.png')"
+
         window.history.pushState({}, "", event.target.href)
         routes.handle()
     })
-    
-    
-    
+
+
+
     elements.oUniverso.addEventListener("click", (event) => {
         event.preventDefault()
-    
+
+
+        elements.BackgroundScreen.style.backgroundImage =
+            "url('./assets/mountains-universe2.png')"
+
         window.history.pushState({}, "", event.target.href)
         routes.handle()
     })
-    
-    
-    
+
+
+
     elements.explorar.addEventListener("click", (event) => {
         event.preventDefault()
-        
+
+
+        elements.BackgroundScreen.style.backgroundImage =
+            "url('./assets/mountains-universe-3.png')"
+
         window.history.pushState({}, "", event.target.href)
         routes.handle()
     })
+
 }
