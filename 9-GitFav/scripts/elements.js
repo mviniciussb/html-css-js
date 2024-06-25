26bf47ade594e5e8f4d462cfaa60e37dbcd45175
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-25 14:55:14 -0300

    .

--- 9-GitFav/scripts/elements.js
+++
@@ -1,4 +0,0 @@
-export const removeUser = document.querySelector(".remove")
-export const userInput = document.querySelector("#user")
-export const buttonSearch = document.querySelector("header form button")
-export const formHeader = document.querySelector("header form")
--- 9-GitFav/scripts/main.js
+++
@@ -1,3 +0,0 @@
-import * as event from "./events.js";
-
-event.removeUser()
