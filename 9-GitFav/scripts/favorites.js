a6ff685beec3a84458cd4398016206f1c338e709
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-06-26 16:10:16 -0300

    .

--- 9-GitFav/scripts/favorites.js
+++ 9-GitFav/scripts/favorites.js
@@ -1,105 +1,159 @@
-// Classe que vai conter a lógica dos dados, ou seja, como os dados serão estruturados
+import { GithubUser } from "./githubUser.js"
 
-export class Favorirites {
+// classe que vai conter a lógica dos dados
+// como os dados serão estruturados
+
+export class Favorites {
+
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+        
     }
+
 
     load() {
-        this.entries = [{
-            login: "",
-            name: "",
-            public_repos: "",
-            follower: ""
-        },
-        {
-            login: "",
-            name: "",
-            public_repos: "",
-            follower: ""
-        }]
+
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+
+        }
+    
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
     }
 
-    delete(user){
-        // Higher-order functions (map, filter, find, reduce)
-        const filteredEntries = this.entries.filter((entry) => entry.login !== user.login)
+
+
+
+    async add(username) {
+
+        const userExists = this.entries.find(entry => entry.login === username)
+
+        if(userExists){
+            throw new Error("Usuário já cadastrado")
+        }
+
+        try {
+            const user = await GithubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+
+            } catch(error) {
+                alert(error.message)
+            }        
+
     }
+
+
+
+    
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
+        this.save()
+    }
+
+
+
 }
 
 
-// Classe que vai criar a visualização e eventos do HTML
 
-export class FavoritesView extends Favorirites {
+// classe que vai criar a visualização e eventos do HTML
+
+export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
-        this.tbody = this.root.querySelector("table tbody")
+
+        this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
     }
 
 
 
+    onadd() {
+        const addButton = this.root.querySelector('.inputButton')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+
+        }
+    }
+    
 
     update() {
         this.removeAllTr()
-
-
-
+        
         this.entries.forEach(user => {
             const row = this.createRow()
+            
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
 
-            row.querySelector(".user img").src = `https://github.com/${user.login}.png`
-            row.querySelector(".user img").alt = `Imagem de ${user.name}`
-            row.querySelector(".user p").textContent = user.name
-            row.querySelector(".user span").textContent = user.login
-            row.querySelector(".repositories").textContent = user.public_repos
-            row.querySelector(".user followers").textContent = user.followers
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
 
-
-            row.querySelector(".remove").onclick = () => {
-                const isOk = confirm("Tem certeza que deseja deletar este perfil?")
-
-                if(isOk){
+                if(isOk) {
                     this.delete(user)
                 }
             }
 
             this.tbody.append(row)
         })
-
+        
+        
         
     }
-
-
 
     createRow() {
-        const tr = document.createElement("tr")
-
+        const tr = document.createElement('tr')
         tr.innerHTML = `
-        
-        <tr>
-            <td class="user">
-                <img src="https://github.com/mviniciussb.png" alt="Foto de Marcos Vinicius">
-                <a href="https://github.com/mviniciussb" target="_blank">
-                    <p>Marcos Vinicius</p>
-                    <span>/mviniciussb</span>
-                </a>
-            </td>
-    
-                <td class="repositories">123</td>
-                <td class="followers">1234</td>
-                <td class="remove"><a href="">Remover</a></td>
-        </tr>
-        
+        <td class="user">
+           <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+           <a href="https://github.com/maykbrito" target="_blank">
+             <p>Mayk Brito</p>
+             <span>maykbrito</span>
+           </a>
+         </td>
+         <td class="repositories">
+           76
+         </td>
+         <td class="followers">
+           9589
+         </td>
+         <td>
+           <button class="remove">Remover</button>
+         </td>
         `
+
         return tr
 
-    }
+        }
+
+
 
 
     removeAllTr() {
-        this.tbody.querySelectorAll("tr").forEach((tr) => {
+
+
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         });
     }
