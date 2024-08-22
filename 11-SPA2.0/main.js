fb981402e730fbc325a62283abc03505d1435af5
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-08-21 22:53:50 -0300

    .

--- 11-SPA2.0/main.js
+++ 11-SPA2.0/main.js
@@ -1,25 +1,29 @@
+const routes = {
+    "/": "./pages/home.html",
+    "/contact": "./pages/contact.html",
+    "/about": "./pages/sobre.html",
+    404: "./pages/404.html"
+};
+
+
 const links = document.querySelectorAll("a")
 
-        links.forEach((link) => {
-            link.addEventListener("click", (evt) => {
-                evt.preventDefault()
+links.forEach((link) => {
+    link.addEventListener("click", (evt) => {
+        evt.preventDefault()
 
-                window.history.pushState({}, "", evt.target.href)
+        window.history.pushState({}, "", evt.target.href)
+        let pathname = window.location.pathname
+        
+        let route = routes[pathname] || routes[404]
 
-            })
+        fetch(route).then(dado => dado.text()).then(dadoText => {
+            document.querySelector("main").innerHTML = dadoText
         })
+     })
+})
 
 
 
 
 
-class Routes{
-    constructor(name, link){
-        this.name = name;
-        this.link = link
-    }
-}
-
-const link1 = new Routes("/", "./pages/home.html")
-const link2 = new Routes("/contato", "./pages/contact.html")
-const link3 = new Routes("/sobre", "./pages/sobre.html")
